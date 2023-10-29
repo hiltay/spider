9c1e3da25ffe0af2c52c50b97f52a3d0324d586f
hiltay <904108079@qq.com>
2023-10-29 18:46:12 +0800

    first commit

--- downloader/target/debug/deps/downloader-44e28d81e8104281.d
+++
@@ -1,5 +0,0 @@
-/Users/yyyz/code/rs/spider/downloader/target/debug/deps/downloader-44e28d81e8104281: src/lib.rs
-
-/Users/yyyz/code/rs/spider/downloader/target/debug/deps/downloader-44e28d81e8104281.d: src/lib.rs
-
-src/lib.rs:
