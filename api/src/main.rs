d27267ac606d0a82824f438dd49f1ec178d7614f
yyyz <904108079@qq.com>
2024-01-01 13:10:59 +0800

    increase axum and tower version

--- api/src/main.rs
+++ api/src/main.rs
@@ -41,9 +41,7 @@ async fn main() {
         // "mongodb" => {}
         _ => return,
     }
-
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
