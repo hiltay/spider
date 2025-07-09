33f90e4a99d01dc72dfa2b9e6658aa5ccd98d3ea
yyyz <904108079@qq.com>
2025-07-09 17:47:34 +0800

    Enhance tracing implementation for better observability

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,39 +5,54 @@ use axum::{
     routing::{get, post},
 };
 use db::{mysql, sqlite};
+use logroller::{Compression, LogRollerBuilder, Rotation, RotationAge, TimeZone};
 use tower::ServiceBuilder;
 use tower_http::cors::{Any, CorsLayer};
+use tower_http::trace::TraceLayer;
 use tracing::info;
+use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::{EnvFilter, fmt, prelude::*, registry::Registry};
-
-fn init_tracing() {
+fn init_tracing()->WorkerGuard {
+    // TODO stdout和file同时输出，并设置不同的fmt
+    // TODO 输出划分为http和core两个文件，似乎可以通过filter来实现 https://docs.rs/tracing-subscriber/latest/tracing_subscriber/layer/index.html
     let formmater_string = "%Y-%m-%d %H:%M:%S (%Z)".to_string();
     let timer = tracing_subscriber::fmt::time::ChronoLocal::new(formmater_string);
+    let appender = LogRollerBuilder::new("./logs", "fcircle.log")
+        .rotation(Rotation::AgeBased(RotationAge::Daily)) // Rotate daily
+        .max_keep_files(7) // Keep a week's worth of logs
+        .time_zone(TimeZone::Local) // Use local timezone
+        .compression(Compression::Gzip) // Compress old logs
+        .build()
+        .unwrap();
+    let (non_blocking, _guard) = tracing_appender::non_blocking(appender);
     let fmt_layer = fmt::layer()
         .with_target(true)
         .with_level(true)
         .with_timer(timer)
         .with_file(true)
         .with_line_number(true)
+        .with_writer(non_blocking)
         .compact();
 
-    let filter = EnvFilter::new("trace");
+    let filter = EnvFilter::new("trace,tower_http=trace,sqlx::query=info");
     tracing_subscriber::registry()
         .with(filter)
         .with(fmt_layer)
         .init();
     info!("Setup tracing success");
+    _guard
 }
 
 #[tokio::main]
 async fn main() {
     let fc_settings = tools::get_yaml_settings("./fc_settings.yaml").unwrap();
-    init_tracing();
+    let _guard = init_tracing();
     let cors = CorsLayer::new()
         .allow_methods(Any)
         .allow_origin(Any)
         .allow_headers(Any);
     let service = ServiceBuilder::new()
+        .layer(TraceLayer::new_for_http())
         .layer(cors);
 
     let app;
